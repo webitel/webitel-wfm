d364f01f9981e6e75f353c5c5d546e0053954f6f
kirychukyurii <me@kirychuk.pp.ua>
2025-03-17 17:20:07 +0200

    ref(grpc/interceptor): remove usage of deprecated protovalidate field func

--- infra/server/interceptor/validate.go
+++ infra/server/interceptor/validate.go
@@ -3,7 +3,10 @@ package interceptor
 import (
 	"context"
 	"errors"
+	"strconv"
+	"strings"
 
+	validatepb "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"github.com/bufbuild/protovalidate-go"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
@@ -21,9 +24,31 @@ func ValidateUnaryServerInterceptor(val *protovalidate.Validator) grpc.UnaryServ
 				if ok := errors.As(err, &ve); ok {
 					wrappers := make([]werror.Wrapper, 0)
 					for _, violation := range ve.Violations {
-						// TODO: deprecated proto message; use new protovalidate.Violation structure
-						// 		 https://github.com/bufbuild/protovalidate-go/releases/tag/v0.8.0
-						wrappers = append(wrappers, werror.WithValue(violation.Proto.GetFieldPath()+"["+violation.Proto.GetConstraintId()+"]",
+						fields := make([]string, 0, len(ve.Violations))
+						for _, f := range violation.Proto.GetField().GetElements() {
+							field := *f.FieldName
+							if f.Subscript != nil {
+								var subscript string
+								switch s := f.Subscript.(type) {
+								case *validatepb.FieldPathElement_Index:
+									subscript = strconv.FormatUint(s.Index, 10)
+								case *validatepb.FieldPathElement_BoolKey:
+									subscript = strconv.FormatBool(s.BoolKey)
+								case *validatepb.FieldPathElement_IntKey:
+									subscript = strconv.FormatInt(s.IntKey, 10)
+								case *validatepb.FieldPathElement_UintKey:
+									subscript = strconv.FormatUint(s.UintKey, 10)
+								case *validatepb.FieldPathElement_StringKey:
+									subscript = s.StringKey
+								}
+
+								field = field + "[" + subscript + "]"
+							}
+
+							fields = append(fields, field)
+						}
+
+						wrappers = append(wrappers, werror.WithValue(strings.Join(fields, ".")+"["+violation.Proto.GetConstraintId()+"]",
 							violation.Proto.GetMessage()),
 						)
 					}
