a59488df0e96e0c85484b0b6d24235c3451ce501
kirychukyurii <me@kirychuk.pp.ua>
2025-03-31 13:24:25 +0300

    ref(interceptor/logger): refactor request logging without checking if err present and log it as debug level

--- infra/server/interceptor/logger.go
+++ infra/server/interceptor/logger.go
@@ -8,15 +8,12 @@ import (
 	"github.com/webitel/webitel-go-kit/logging/wlog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
-
-	"github.com/webitel/webitel-wfm/infra/server/grpccontext"
 )
 
 // LoggingUnaryServerInterceptor returns a new unary server interceptor for logging requests.
 func LoggingUnaryServerInterceptor(log *wlog.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		start := time.Now()
-
 		ip := "<not found>"
 		if md, ok := metadata.FromIncomingContext(ctx); ok {
 			ip = getClientIp(md)
@@ -24,25 +21,13 @@ func LoggingUnaryServerInterceptor(log *wlog.Logger) grpc.UnaryServerInterceptor
 
 		h, err := handler(ctx, req)
 
-		// var f wlog.Field
-		s := grpccontext.FromContext(ctx)
-		if err != nil {
-			f := wlog.Err(err)
-			// var appError apperrors.AppError
-
-			// switch {
-			// case errors.As(err, &appError):
-			// 	// f = wlog.ObjectErr(appError.SetRequestId(s.RequestId))
-			// default:
-			// 	f = wlog.Err(err)
-			// }
-
-			log.Error("processed request", f, wlog.String("client_ip", ip),
-				wlog.Any("method", info.FullMethod), wlog.Any("duration", time.Since(start)))
-		} else {
-			log.Debug("processed request", wlog.String("client_ip", ip),
-				wlog.Any("method", info.FullMethod), wlog.Any("duration", time.Since(start)), wlog.String("request_id", s.RequestId))
-		}
+		// TODO: Client errors (that do not appear as an application logic error)
+		// 	should be logged as DEBUG or INFO level.
+		log.Debug("processed request", wlog.Err(err),
+			wlog.String("client_ip", ip),
+			wlog.Any("method", info.FullMethod),
+			wlog.String("duration", time.Since(start).String()),
+		)
 
 		return h, err
 	}
