b6ea89efeb8b081394be79fcbbb8bd2910f01f7d
kirychukyurii <me@kirychuk.pp.ua>
2025-04-07 15:49:19 +0300

    ref(tool): run with go tool instead of binary

--- webitel-wfm.go
+++ webitel-wfm.go
@@ -6,9 +6,9 @@ import (
 	"github.com/webitel/webitel-wfm/cmd"
 )
 
-//go:generate buf generate --template buf.gen.yaml
-//go:generate mockery
-//go:generate wire gen ./cmd
+//go:generate go tool buf generate --template buf.gen.yaml
+//go:generate go tool mockery
+//go:generate go tool wire gen ./cmd
 
 func main() {
 	if err := cmd.Run(); err != nil {
