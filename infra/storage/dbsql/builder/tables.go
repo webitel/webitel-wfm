c244700f58297cc895c771496ae75685cd9fcef0
kirychukyurii <me@kirychuk.pp.ua>
2025-04-12 10:58:25 +0300

    ref(working_schedule): remove view usages and build sql using builder

--- infra/storage/dbsql/builder/tables.go
+++ infra/storage/dbsql/builder/tables.go
@@ -5,18 +5,27 @@ var (
 )
 
 var (
-	PauseCauseTable = Table{name: "call_center.cc_pause_cause", alias: "cpc"}
-	AgentTable      = Table{name: "call_center.cc_agent", alias: "ca"}
+	CalendarTable = Table{name: "flow.calendar", alias: "cal"}
 )
 
 var (
-	PauseTemplateTable         = Table{name: "wfm.pause_template", alias: "pt"}
-	PauseTemplateCauseTable    = Table{name: "wfm.pause_template_cause", alias: "ptc"}
-	ShiftTemplateTable         = Table{name: "wfm.shift_template", alias: "st"}
-	WorkingConditionTable      = Table{name: "wfm.working_condition", alias: "wc"}
-	AgentWorkingConditionTable = Table{name: "wfm.agent_working_condition", alias: "awc"}
-	AgentAbsenceTable          = Table{name: "wfm.agent_absence", alias: "aa"}
-	ForecastCalculationTable   = Table{name: "wfm.forecast_calculation", alias: "fc"}
+	PauseCauseTable = Table{name: "call_center.cc_pause_cause", alias: "cpc"}
+	AgentTable      = Table{name: "call_center.cc_agent", alias: "ca"}
+	TeamTable       = Table{name: "call_center.cc_team", alias: "ct"}
+	SkillTable      = Table{name: "call_center.cc_skill", alias: "cs"}
+)
+
+var (
+	PauseTemplateTable             = Table{name: "wfm.pause_template", alias: "pt"}
+	PauseTemplateCauseTable        = Table{name: "wfm.pause_template_cause", alias: "ptc"}
+	ShiftTemplateTable             = Table{name: "wfm.shift_template", alias: "st"}
+	WorkingConditionTable          = Table{name: "wfm.working_condition", alias: "wc"}
+	AgentWorkingConditionTable     = Table{name: "wfm.agent_working_condition", alias: "awc"}
+	AgentAbsenceTable              = Table{name: "wfm.agent_absence", alias: "aa"}
+	ForecastCalculationTable       = Table{name: "wfm.forecast_calculation", alias: "fc"}
+	WorkingScheduleTable           = Table{name: "wfm.working_schedule", alias: "ws"}
+	WorkingScheduleExtraSkillTable = Table{name: "wfm.working_schedule_extra_skill", alias: "wses"}
+	WorkingScheduleAgentTable      = Table{name: "wfm.working_schedule_agent", alias: "wsa"}
 )
 
 type Table struct {
